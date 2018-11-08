e7f8a3696b31c5d678bfd3b40e78427b751aa489
botbiz <vinicius.bizarri@gmail.com>
2018-11-08 00:56:53 -0200

    initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# metainf
-metainf is a database metadata knwoledge centralizer (in progress).
---
+++ metainf/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace metainf.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
