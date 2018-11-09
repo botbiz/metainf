2ba0aa46400b7aec9a566c578153842c9969a7e3
botbiz <vinicius.bizarri@gmail.com>
2018-11-08 22:25:42 -0200

    Created connections tab and grid, new controllers and added bootstrap-table.

---
+++ metainf/Controllers/SettingsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using metainf.Models;
+
+namespace metainf.Controllers
+{
+    public class SettingsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Connections()
+        {
+            ViewData["Message"] = "All saved database connections";
+
+            return View();
+        }
+
+        public IActionResult Users()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
